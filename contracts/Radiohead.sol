5b6dd0f2a146de70f81b8158c0e271f1ef4fc0e2
jarryingnut <rabhishekranjan@gmail.com>
2023-02-02 01:15:22 +0530

    add validations and royality parameters

--- contracts/Radiohead.sol
+++ contracts/Radiohead.sol
@@ -19,12 +19,21 @@ contract Radiohead is ERC1155, Ownable, ERC1155Supply {
         address artist;
         uint songId;
         uint ltdSongId;
-        uint256 limitedSupply;
+        uint limitedSupply;
+        uint limitedSongMinted;
         uint regularPrice;
         uint limitedPrice;
+        uint regularRevenue;
+        uint ltdRevenue;
+        uint platformRoyality;
+        uint artistRoyality;
+        uint superfanRoyality;
+        address[] superfans;
     }
 
-    mapping(uint => Song) public songs; // we are using regular song Id's to map songs. these are all odd numbers.
+    mapping(uint => Song) public songs;
+
+    // we are using regular song Id's to map songs. these are all odd numbers.
 
     constructor(address _escrow) ERC1155("") {
         _songIdCounter.increment();
@@ -39,7 +48,10 @@ contract Radiohead is ERC1155, Ownable, ERC1155Supply {
     function createSong(
         uint _limitedSupply,
         uint _regularSongPrice,
-        uint _limitedSongPrice
+        uint _limitedSongPrice,
+        uint _platformRoyality,
+        uint _artistRoyality,
+        uint _superfanRoyality
     ) external {
         require(_limitedSupply > 0, "Limited supply must be greater than 0");
         uint songId = _songIdCounter.current();
@@ -51,6 +63,9 @@ contract Radiohead is ERC1155, Ownable, ERC1155Supply {
         currentSong.limitedSupply = _limitedSupply;
         currentSong.regularPrice = _regularSongPrice;
         currentSong.limitedPrice = _limitedSongPrice;
+        currentSong.platformRoyality = _platformRoyality;
+        currentSong.artistRoyality = _artistRoyality;
+        currentSong.superfanRoyality = _superfanRoyality;
 
         _songIdCounter.increment();
 
@@ -65,29 +80,52 @@ contract Radiohead is ERC1155, Ownable, ERC1155Supply {
         }
     }
 
-    function buyRegularSong(uint songId) external {
-        //safeTransferFrom(address(this), msg.sender, songId, 1, "");
+    function buyRegularSong(uint songId) external payable {
+        Song storage currentSong = songs[songId];
+        require(exists(songId), "the songId doesn't exists or not valid");
+        require(
+            msg.value >= currentSong.regularPrice,
+            "Please pay the full amount"
+        );
+        currentSong.regularRevenue += msg.value;
         escrow.buyRegularSong(
             songId,
             payable(address(this)),
-            songs[songId].artist,
+            currentSong.artist,
             msg.sender
         );
     }
 
-    function buyLimitedSong(uint songId) external {
-        uint ltdSongId = songs[songId].ltdSongId;
+    function buyLimitedSong(uint songId) external payable {
+        Song storage currentSong = songs[songId];
+        require(
+            exists(currentSong.ltdSongId),
+            "the songId doesn't exists or not valid"
+        );
+        require(currentSong.limitedSongMinted < currentSong.limitedSupply, "");
+        require(
+            msg.value >= currentSong.limitedPrice,
+            "Please pay the full amount"
+        );
+        currentSong.limitedSongMinted += 1;
+        currentSong.ltdRevenue += msg.value;
+        currentSong.superfans.push(msg.sender);
         escrow.buyRegularSong(
-            ltdSongId,
+            currentSong.ltdSongId,
             payable(address(this)),
-            songs[songId].artist,
+            currentSong.artist,
             msg.sender
         );
     }
 
-    // function withdraw() external {
-
-    // }
+    function withdrawRoyalities() external {
+        // iterate a loop till _songIdCounter.current(), regular song id's are always odd so keep that in mind
+        //go through every song in songs mapping
+        // check if the song exists and if the current song is limited or reguar
+        //calculate royalities per song for different parties
+        //distribute royalities to different parties
+        // reset all revenues
+    }
 
     // The following functions are overrides required by Solidity.
 

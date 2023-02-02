396aa3ddb05dfc5c36e475154442a4c6cf3d1a9c
jarryingnut <rabhishekranjan@gmail.com>
2023-02-02 16:38:42 +0530

    impliment the rough withdraw function

--- contracts/Radiohead.sol
+++ contracts/Radiohead.sol
@@ -9,8 +9,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Radiohead is ERC1155, Ownable, ERC1155Supply {
     using Counters for Counters.Counter;
-    Counters.Counter private _songIdCounter;
-    address private owner;
+    Counters.Counter private songIdCounter;
     Escrow private escrow;
 
     // A single song will have two songIds, one belongs to unlimited edition and anoter one belongs to limited ones
@@ -35,8 +34,7 @@ contract Radiohead is ERC1155, Ownable, ERC1155Supply {
     // we are using regular song Id's to map songs. these are all odd numbers.
 
     constructor(address _escrow) ERC1155("") {
-        _songIdCounter.increment();
-        owner = msg.sender;
+        songIdCounter.increment();
         escrow = Escrow(_escrow);
     }
 
@@ -52,7 +50,7 @@ contract Radiohead is ERC1155, Ownable, ERC1155Supply {
         uint _superfanRoyality
     ) external {
         require(_limitedSupply > 0, "Limited supply must be greater than 0");
-        uint songId = _songIdCounter.current();
+        uint songId = songIdCounter.current();
         _mint(msg.sender, songId, 10 ** 18, "");
 
         Song storage currentSong = songs[songId];
@@ -64,14 +62,14 @@ contract Radiohead is ERC1155, Ownable, ERC1155Supply {
         currentSong.platformRoyality = _platformRoyality;
         currentSong.superfanRoyality = _superfanRoyality;
 
-        _songIdCounter.increment();
+        songIdCounter.increment();
 
-        uint ltdSongId = _songIdCounter.current();
+        uint ltdSongId = songIdCounter.current();
         _mint(msg.sender, ltdSongId, _limitedSupply, "");
 
         currentSong.ltdSongId = ltdSongId;
 
-        _songIdCounter.increment();
+        songIdCounter.increment();
         if (!isApprovedForAll(msg.sender, address(escrow))) {
             setApprovalForAll(address(escrow), true);
         }
@@ -116,7 +114,56 @@ contract Radiohead is ERC1155, Ownable, ERC1155Supply {
     }
 
     function withdrawRoyalities() external {
-        // iterate a loop till _songIdCounter.current(), regular song id's are always odd so keep that in mind
+        uint totalSongs = songIdCounter.current();
+        for (uint i = 1; i <= totalSongs; i += 2) {
+            Song storage currentSong = songs[i];
+            require(exists(i), "the song doesnt exists");
+
+            // for regular songs
+            uint platformRevenueRegular = (currentSong.regularRevenue / 100) *
+                currentSong.platformRoyality;
+            uint artistRevenueRegular = currentSong.regularRevenue -
+                platformRevenueRegular;
+
+            // for limited songs
+            uint platformRevenueLtd = (currentSong.ltdRevenue / 100) *
+                currentSong.platformRoyality;
+            uint artistRevenueLtd = currentSong.ltdRevenue - platformRevenueLtd;
+
+            //calculate the limited edition songs to superfans
+            address[] memory superfans = currentSong.superfans;
+            uint microRevenue = ((currentSong.regularRevenue / 100) *
+                currentSong.superfanRoyality) / currentSong.limitedSupply;
+            uint superfanRevenue = microRevenue * superfans.length;
+
+            // total artist revenue
+            uint artistRevenue = artistRevenueRegular +
+                artistRevenueLtd -
+                superfanRevenue;
+
+            // reset the revenue balance on the song to reflect the distribution
+            currentSong.regularRevenue = 0;
+            currentSong.ltdRevenue = 0;
+
+            //distribute revenue to the artist
+            (bool sentArtist, ) = payable(currentSong.artist).call{
+                value: artistRevenue
+            }("");
+            require(sentArtist, "failed to disribute royalities to the artist");
+
+            //revenue for the platform will stay in the contract
+
+            //distribute revenue to the superfans
+            for (uint j = 0; j < superfans.length; j++) {
+                address payable superfan = payable(superfans[i]);
+                (bool sentMicro, ) = superfan.call{value: microRevenue}("");
+                require(
+                    sentMicro,
+                    "failed to disribute royalities to the superfan"
+                );
+            }
+        }
+        // iterate a loop till songIdCounter.current(), regular song id's are always odd so keep that in mind
         //go through every song in songs mapping
         // check if the song exists and if the current song is limited or reguar
         //calculate royalities per song for different parties

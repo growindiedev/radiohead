4a9a4f4db13ac1d6667558cf0b405f72be9c2b60
jarryingnut <rabhishekranjan@gmail.com>
2023-02-12 17:49:13 +0530

    deploy contract to goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+	solidity: "0.8.17",
+	networks: {
+		goerli: {
+			url: process.env.ALCHEMY_GOERLI_URL,
+			accounts: [process.env.GOERLI_PRIVATE_KEY],
+		},
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_KEY,
+	},
 };
--- radiohead-frontend/tailwind.config.js
+++ radiohead-frontend/tailwind.config.js
@@ -16,6 +16,6 @@ module.exports = {
 		//themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine",
 		//"halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula",
 		//"cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
-		themes: ["cyberpunk"],
+		themes: ["luxury"],
 	},
 };

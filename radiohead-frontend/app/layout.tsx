be9a10ddbad96a8750c43ffea96afe1fc4058099
jarryingnut <rabhishekranjan@gmail.com>
2023-02-20 16:59:26 +0530

    add badges for limited/regular song in playlist

--- radiohead-frontend/app/layout.tsx
+++ radiohead-frontend/app/layout.tsx
@@ -88,16 +88,24 @@ export default function RootLayout({
 											data-amplitude-song-index="0"
 										>
 											<div className="avatar">
-												<div className="mask mask-squircle w-14 h-14">
+												<div className="mask mask-squircle w-16 h-16">
 													<img src="https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg" />
 												</div>
 											</div>
 											<div>
-												<div className="font-bold">Risin' High</div>
+												<div className="font-bold text-sm">
+													Risin' High ding dong
+												</div>
 												{/* <div className="text-sm opacity-50 animate-marquee whitespace-nowrap overflow-hidden absolute"> */}
-												<marquee>
+												<marquee className="text-xs">
 													Ancient Astronauts &bull; We Are to Answer
 												</marquee>
+												<div className="flex gap-1">
+													<div className="badge badge-xs badge-secondary">
+														limited: 5
+													</div>
+													<div className="badge badge-xs">regular: 1</div>
+												</div>
 											</div>
 										</div>
 									</div>

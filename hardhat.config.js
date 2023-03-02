f2e9e05595211d45851320d0cf6910aa335c714f
jarryingnut <rabhishekranjan@gmail.com>
2023-03-02 18:22:27 +0530

    write full tests for withdrawl

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("solidity-coverage");
 
 module.exports = {
 	solidity: "0.8.17",
 	networks: {
+		// hardhat: {
+		// 	gasPrice: 0,
+		// 	gas: 0,
+		// 	gasMultiplier: 0,
+		// },
 		goerli: {
 			url: process.env.ALCHEMY_GOERLI_URL,
 			accounts: [process.env.GOERLI_PRIVATE_KEY],
